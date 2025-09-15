9a15bfff600834590c835ad29955774651c6fab6
heenakhan4 <khanheena4997@gmail.com>
2025-09-15 15:03:28 +0530

    Deployment: gh-pages setup

--- src/components/QRScanner.tsx
+++ src/components/QRScanner.tsx
@@ -73,7 +73,7 @@ export const QRScanner = () => {
 
         toast({
           title: "Scan Successful!",
-          description: `Captured data for ${result.data.name}`,
+          description: `Captured data for ${result.data}`,
         });
       } catch (error) {
         let h = JSON.stringify(result, null, 2)
